f63c72c4b61af2cd50da7dc6d13d7e3a2fbd58c0
PrashamW <cs21btech11047@iith.ac.in>
2023-06-10 13:11:20 +0530

    Projct upload

--- projects/QnA_website/backend/controllers/posts.js
+++ projects/QnA_website/backend/controllers/posts.js
@@ -15,16 +15,16 @@ const queryMaker = (array,option)=>{
     var i;
     for(i=0;i<arr.length;i++){
         if(i == 0){
-            newString = string.concat(`tags like '%${arr[i]}%' and `);
+            newString = string.concat(`tags like '%<${arr[i]}>%' and `);
         }
         else{
-            newString = newString.concat(`tags like '%${arr[i]}%' and `);
+            newString = newString.concat(`tags like '%<${arr[i]}>%' and `);
         }
     }
     const length = newString.length;
     newString = newString.slice(0,length-5);
     if(option === 'time'){
-        newString = newString.concat(" order by creation_date asc");
+        newString = newString.concat(" order by creation_date desc");
     }
     else if(option === 'upvotes'){
         newString = newString.concat(" order by score desc");
@@ -32,7 +32,7 @@ const queryMaker = (array,option)=>{
     return newString;
 }
 
-export const getPostById = async (req,res)=>{
+export const getPostsById = async (req,res)=>{
     try {
         const {searchBy,array,option} = req.body;
         var getPosts;
@@ -44,7 +44,7 @@ export const getPostById = async (req,res)=>{
         }
         else{
             if(option === 'time'){
-                getPosts = await pool.query("select * from posts where owner_user_id = $1 order by creation_date asc",[array]);
+                getPosts = await pool.query("select * from posts where owner_user_id = $1 order by creation_date desc",[array]);
             }
             else if(option === 'upvotes'){
                 getPosts = await pool.query("select * from posts where owner_user_id = $1 order by score desc",[array]);
@@ -74,10 +74,112 @@ export const getTop = async (req,res)=>{
     }
 }
 
-export const getPostByTag = async ()=>{
+export const getPostByTag = async (req,res)=>{
     try {
+        // console.log("here")
+        const {tag} = req.params;
+        const query = "%"+tag+"%";
+        const getPosts = await pool.query("select * from posts where tags like $1",[query]);
+        res.json(getPosts.rows);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const TagExtractor = (tags)=>{
+   var tagString = "";
+   tags.forEach((el)=>{
+    tagString = tagString.concat(`<${el.tag_name}>`)
+   })
+
+   return tagString;
+}
+
+export const addPostByID = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, tags, body, postTypeId } = req.body;
+
+        const tagString = TagExtractor(tags);
+        // console.log(tagString);
+
+        const newPost = await pool.query("INSERT INTO posts(owner_user_id,post_type_id,score,title,tags,content_license,body,creation_date) values($1,$5,0,$2,$3,'No licencse',$4,now()) RETURNING *;",[id,title,tagString,body,postTypeId]);
+
+        res.json(newPost.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+};
+
+export const addPostByID2 = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { body, postTypeId, parentId } = req.body;
+
+        const newPost = await pool.query("INSERT INTO posts(owner_user_id,post_type_id,score,parent_id,content_license,body,creation_date) values($1,$2,0,$3,'No licencse',$4,now()) RETURNING *;",[id,postTypeId,parentId,body]);
+
+        res.json(newPost.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+};
+
+export const editPostById = async (req,res)=>{
+    try {
+        const {title,body,tags} = req.body;
+        const post_id = req.params;
+        const response = await pool.query("update posts set title = $1,body = $2,tags = $3 where id = $4",[title,body,TagExtractor(tags),post_id.post_id]);
+        res.sendStatus(200);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getPostbyId =  async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const getPost = await pool.query("select * from posts where id = $1",[id]);
+        // console.log(getPost.rows);
+        res.send(getPost.rows);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getOwnPosts = async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const response = await pool.query('select * from posts where owner_user_id = $1 and post_type_id=1',[id]);
+        // console.log(response.rows);
+        res.json(response.rows);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deletePost = async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const response = await pool.query('delete from posts where id = $1',[id]);
+        if(response.rowCount !== 0){
+            console.log("here")
+           return res.sendStatus(200);
+        }
+        return res.sendStatus(403);
         
     } catch (error) {
         console.log(error);
     }
 }
+
+export const getPostAnswersbyId =  async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const getPost = await pool.query("select * from posts where parent_id = $1 and post_type_id = '2' order by score desc",[id]);
+        // console.log(getPost.rows);
+        res.send(getPost.rows);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
--- projects/QnA_website/try.js
+++ projects/QnA_website/try.js
@@ -1,4 +1,23 @@
-var regularExpression= /(?<=\[).*?(?=\])/g;
-var getTheValueWithIndex= "[John Smith][David Miller]".match(regularExpression);
-console.log("The first value without bracket="+getTheValueWithIndex[0]);
-console.log("The second value without bracket="+getTheValueWithIndex[1]);
+const bcrypt = require('bcrypt')
+
+const x = "Maharshi"
+const saltRounds = 10;
+
+const hash_pw = async ()=>{
+    const salt = await bcrypt.genSalt(10);
+    const encrypted = await bcrypt.hash(x,salt);
+
+    console.log(encrypted);
+
+    const compare  =bcrypt.compare(x,encrypted);
+    if(compare){
+        console.log("matched");
+    }
+    else{
+        console.log("NO");
+    }
+}
+
+hash_pw();
+
+
